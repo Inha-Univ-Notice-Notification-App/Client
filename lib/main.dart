bbf8de161737a4a948fcd7ba92b76cc07deda78d
Nagyeom An <nagyeoman@Nagyeomui-MacBookPro.local>
2022-08-02 23:56:58 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const SettingHome());
+  runApp(const SettingAlarmOff());
 }
 // loading1
 class Loading1 extends StatelessWidget {
@@ -58,22 +58,82 @@ class Loading2 extends StatelessWidget {
     );
   }
 }
-// setting_home
+
+// setting_button -> show SettingHome Dialog
+class SettingButton extends StatelessWidget {
+  const SettingButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.density_medium_rounded),
+      tooltip: 'Show Settings',
+      onPressed: (){
+        showDialog(context: context, builder: (context){
+          return SettingHome();
+        });
+      },
+    );
+  }
+}
+
+
+// setting_home -> Dialog // must have MaterialApp as a parent
 class SettingHome extends StatelessWidget {
   const SettingHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Dialog(
+      child: SizedBox(
+        // how to align this left??
+        height: double.infinity,
+        width: 200, // have to change
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_new_rounded), // three dots
+              onPressed:(){} // should put Home_page1
+            )
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(onPressed: (){},// change screen to alarm setting
+                  child: ListTile(
+                      leading: Text('알람설정'),
+                      title: Icon(Icons.arrow_forward_ios_rounded)
+                  )),
+              ElevatedButton(onPressed: (){},// change screen to alarm setting
+                  child: ListTile(
+                      leading: Text('즐겨찾기'),
+                      title: Icon(Icons.arrow_forward_ios_rounded)
+                  )),
+              ElevatedButton(onPressed: (){},// change screen to alarm setting
+                  child: ListTile(
+                      leading: Text('팀 오늘의 인하'),
+                      title: Icon(Icons.arrow_forward_ios_rounded)
+                  ))
+            ]
+          )
+        )
+    )
+    );
   }
 }
 
+// setting_alarm_off
 class SettingAlarmOff extends StatelessWidget {
   const SettingAlarmOff({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container()
+      )
+    );
   }
 }
 
@@ -104,3 +164,15 @@ class TeamIntro extends StatelessWidget {
   }
 }
 
+// upper part of setting
+class SettingTop extends StatelessWidget {
+  const SettingTop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      actions:[Icon(Icons.arrow_back_ios_new_rounded)],
+        title: Text('알림 설정', style: TextStyle(fontWeight: FontWeight.bold))
+    );
+  }
+}
