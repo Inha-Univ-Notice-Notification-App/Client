53945084806cc34c4be42db672ce0d40eea799ac
이창용 <104843976+era-s@users.noreply.github.com>
2022-08-13 20:31:14 +0900

    initial

---
+++ android/app/src/main/kotlin/com/example/inha_notice_igrus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inha_notice_igrus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/root_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:inha_notice_igrus/home_page.dart';
+import 'package:inha_notice_igrus/tags_page.dart';
+import 'package:inha_notice_igrus/trend_page.dart';
+import 'like_page.dart';
+
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    HomePage(),
+    TrendPage(),
+    LikePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: GNav(
+            backgroundColor: Colors.white ,
+            tabBackgroundColor: Colors.grey,
+            gap: 8,
+            onTabChange: (index) {
+              _navigateBottomBar(index);
+            },
+            padding: EdgeInsets.all(8),
+            tabs: [
+              GButton(icon: Icons.home,
+              text: 'Home'),
+              GButton(icon: Icons.trending_up,
+              text: 'trend'),
+              GButton(icon: Icons.favorite,
+              text: 'like',),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
